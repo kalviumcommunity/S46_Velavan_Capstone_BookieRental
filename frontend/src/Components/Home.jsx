382755de9f379359f8f89b2bf54cea54efa70145
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2024-04-02 17:24:39 +0530

    Static landing page created

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import logo from '../assets/1.png';
+
+const Home = () => {
+
+
+    
+  return (
+    <>
+      
+      <div className='navbar flex items-center justify-between bg-red-300 h-[6.5rem]'>
+    
+        <img src={logo} className="h-44 w-44" alt="Logo" />
+
+        <input type="text" placeholder='Search...' className="px-2 py-1 bg-white text-red-500 rounded-xl focus:outline-none text-center w-64"/>
+
+        <button className="bg-white hover:bg-red-600 hover:text-white text-red-600 font-bold px-4 py-2 rounded-xl w-28 m-7">
+          LogIn
+        </button>
+      
+      </div>
+
+      <div className='w-screen h-screen bg-red-200'>
+        <h1 className='text-center text-4xl font-bold'>Welcome to BookieRental</h1>
+        <p className='text-center text-xl font-bold mt-5'>Find your next book</p>
+        <p className='text-center text-xl font-bold mt-5'>Rent your favourite books</p>
+        <p className='text-center text-xl font-bold mt-5'>And more...</p>
+        <p className='text-center text-xl font-bold mt-5'>Enjoy your stay</p>
+        <p className='text-center text-xl font-bold mt-5'>And don't forget to rate us</p>
+      </div>
+
+    </>
+  )
+}
+
+export default Home
